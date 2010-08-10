e4bb04da46335fb1df64ec57a30053cb869955b0
John Cantrell <jdcantrell@gmail.com>
2010-08-10 15:28:04 -0500

    Removed item editing, implemented dnd of lists between columns

--- vimes.js
+++ vimes.js
@@ -1,69 +1,51 @@
-function addItem(event) {
-	var item = $('<li>New Item</li>')
-	item.bind({
-		mouseover: highlightListItem,
-		mouseout: unhighlightListItem,
-		click: editItem
+function updateList(e) {
+	var text = this.value
+	$('#list').html('<li>' + text.replace(/\n/g,"<li>"))
+}
+
+function editList() {
+	var list = $(this).prev()
+	var position = $('#list').offset()
+	var editor = $('#listEditor')
+	list.addClass('editing')
+	editor.html(list.html().replace("<li>","").replace(/\s+/g," ").replace(/<li>/g, "\n").replace(/<\/li>/g, ""))
+	$('#listEditor').css({
+		left: position.left+ 28,
+		top: position.top - 2,
+		display: 'block',
+		position: 'fixed',
+		width: list.children().first().outerWidth()
 	})
-	item.insertBefore(this)
-	item.triggerHandler('click')
+	$('#listEditor').focus()
+
 }
 
-currentItem = null
-function highlightListItem(event) {
-	var tools = $('#itemToolTray')
-	var pos = $(this).position()
-	var width = $(this).width()
-	pos.left += width
-	pos.position = 'absolute'
-	console.log(pos)
-	tools.css(pos)
-	currentItem = this
-}
-function unhighlightListItem(event) {}
 
-var editItems = []
 
-function editComplete(item) {
-	item.parent().removeClass('current-edit-item')
-	item.parent().text(item.attr('value'))
-}
-
-function editItem(item) {
-	console.log('edit', item)
-	if ($(item).hasClass('current-edit-item')) { return }
-	$.each(editItems, function(idx, value) { editComplete(value)})
-	$(item).addClass('current-edit-item')
-	var value = $(item).text()	
-	var input = $("<input type='text' value='" + value + "'>")
-	$(item).html("")
-	input.appendTo($(item))
-	input.focus()
-	editItems[editItems.length] = input
+function completeEdit() {
+	var text = this.value
+	$('#list').html('<li>' + text.replace(/\n/g,"<li>"))
+	$(this).css('display','none')
 }
 
 $(document).ready(function() {
-	$('#editItem').click(function() {editItem(currentItem)})
-	$(".list-area li").each(function(index) {
-		$(this).bind({
-			mouseover: highlightListItem,
-			mouseout: unhighlightListItem,
-		})
-	})
-	$(".list-area h1").each(function(index) {
-		$(this).bind({
-			mouseover: highlightListItem,
-			mouseout: unhighlightListItem,
-		})
-	})
-	$(".list-area ul").each(function(index) {
-		var add = $('<li class="create-item">Add Item</li>')
-		add.click(addItem)
-		add.appendTo(this)
-		$(this).bind({
-			mouseover: function() {$(this).addClass("current-list")},
-			mouseout: function() {$(this).removeClass("current-list")}
-		})
+	$('.edit-link').bind('click', editList)
+	$('#listEditor').bind('keyup', updateList)
+	$('#listEditor').bind('blur', completeEdit)
+	$('.column').sortable({
+		connectWith:'.column',
+		handle:'h1',
+		sort:function(e, ui) {
+			var minHeight = 0
+			$('.column').each(function (idx, col) {
+				$('.column').css('min-height', 0)
+				var h = $(col).height()
+				console.log(h)
+				minHeight = (h > minHeight) ? h : minHeight
+			})
+			console.log('min-height',minHeight)
+			$('.column').css('minHeight',minHeight)
+		}
 	})
 
 
