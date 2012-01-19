cb46ae64df0126a7b0bac5a286c896364134b029
JD Cantrell <jdcantrell@gmail.com>
2012-01-18 21:56:05 -0800

    Update config file to something that might work

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
-class Config(object):
-    DEBUG = False
-    DB_HOST = "localhost"
-    DB_USER = "db_user"
-    DB_PASSWORD = "db_password"
-    DB_DATABASE = "database"
+#To get more informative error messages set this True
+DEBUG = True
+
+#DB Settings
+DB_TYPE = "mysql"
+DB_DRIVER = ""
+DB_PORT = ""
+DB_HOST = "localhost"
+DB_USER = "change me"
+DB_PASSWORD = "change me"
+DB_DATABASE = "change me"
+
+#If this is set to True, then we will not use usernames in urls
+#otherwise usernames will be used when visiting some pages.
+SINGLE_USER = True
+
+#OID Secret key
+SECRET_KEY = 'change me'
