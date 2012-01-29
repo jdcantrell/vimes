78f7a5154d98cafddbeb6d3a4a4f2084612e0273
JD Cantrell <jdcantrell@gmail.com>
2012-01-29 00:57:41 -0800

    The config now has more flexible database settings

--- config.py
+++ config.py
@@ -2,17 +2,11 @@
 DEBUG = True
 
 #DB Settings
-DB_TYPE = "mysql"
-DB_DRIVER = ""
-DB_PORT = ""
-DB_HOST = "localhost"
-DB_USER = "change me"
-DB_PASSWORD = "change me"
-DB_DATABASE = "change me"
+SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/vimes.db"
 
 #If this is set to True, then we will not use usernames in urls
 #otherwise usernames will be used when visiting some pages.
 SINGLE_USER = True
 
-#OID Secret key
+#Secret key
 SECRET_KEY = 'change me'
